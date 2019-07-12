64689c7f0f7294b14764aabf7aa3b37fd82ba731
Ichinose Shogo <shogo82148@gmail.com>
2019-07-12 11:30:51 +0900

    add test for api gateway get requrst

--- ridgenative_test.go
+++ ridgenative_test.go
@@ -117,6 +117,38 @@ func TestHTTPRequest(t *testing.T) {
 			t.Errorf("unexpected host: want %q, got %q", "lambda-test-1234567890.ap-northeast-1.elb.amazonaws.com", httpReq.Host)
 		}
 	})
+	t.Run("api gateway get request", func(t *testing.T) {
+		req, err := loadRequest("testdata/apigateway-get-request.json")
+		if err != nil {
+			t.Fatal(err)
+		}
+		httpReq, err := httpRequest(context.Background(), req)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if httpReq.Method != http.MethodGet {
+			t.Errorf("unexpected method: want %s, got %s", http.MethodGet, httpReq.Method)
+		}
+		if !reflect.DeepEqual(httpReq.Header["Header-Name"], []string{"Value1", "Value2"}) {
+			t.Errorf("unexpected header: want %v, got %v", []string{"Value1", "Value2"}, httpReq.Header["Header-Name"])
+		}
+		if httpReq.RequestURI != "/foo%20/bar?query=hoge&query=fuga" {
+			t.Errorf("unexpected RequestURI: want %q, got %q", "/foo%20/bar?query=hoge&query=fuga", httpReq.RequestURI)
+		}
+		if httpReq.ContentLength != 0 {
+			t.Errorf("unexpected ContentLength: want %d, got %d", 0, httpReq.ContentLength)
+		}
+		body, err := ioutil.ReadAll(httpReq.Body)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if string(body) != "" {
+			t.Errorf("unexpected body: want %q, got %q", "", string(body))
+		}
+		if httpReq.Host != "xxxxxxxxxx.execute-api.ap-northeast-1.amazonaws.com" {
+			t.Errorf("unexpected host: want %q, got %q", "xxxxxxxxxx.execute-api.ap-northeast-1.amazonaws.com", httpReq.Host)
+		}
+	})
 }
 
 func Benchmark(b *testing.B) {
